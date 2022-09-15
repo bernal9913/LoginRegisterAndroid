05a14f018c78791e1fcb21b3305f23839b625184
Carlos Bernal <ramirez.bernal.jesus1@gmail.com>
2022-09-15 12:04:55 -0700

    spinner added

--- app/src/main/java/com/example/login/RegisterActivity.java
+++ app/src/main/java/com/example/login/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +21,7 @@ public class RegisterActivity extends AppCompatActivity {
     Button register;
     // DatePicker date;
     dbStopJumper db;
+    Spinner nation1;
     TextView login;
 
     @Override
@@ -31,7 +33,7 @@ public class RegisterActivity extends AppCompatActivity {
         pass = findViewById(R.id.tf_registerPass);
         repass = findViewById(R.id.tf_registerRepass);
         email = findViewById(R.id.tf_registerMail);
-        nation = findViewById(R.id.tf_registerOrigin);
+        nation1 = findViewById(R.id.tf_registerOrigin1);
         date = findViewById(R.id.dp_birthDate);
         register = findViewById(R.id.btn_register);
         login = findViewById(R.id.tv_log);
@@ -44,7 +46,7 @@ public class RegisterActivity extends AppCompatActivity {
                 String p = pass.getText().toString();
                 String rp = repass.getText().toString();
                 String e = email.getText().toString();
-                String n = nation.getText().toString();
+                String n = nation1.getSelectedItem().toString();
                 String d = date.getText().toString();
                 if(TextUtils.isEmpty(u) || TextUtils.isEmpty(p) || TextUtils.isEmpty(e) || TextUtils.isEmpty(n) || TextUtils.isEmpty(d) || TextUtils.isEmpty(rp)){
                     Toast.makeText(RegisterActivity.this,
