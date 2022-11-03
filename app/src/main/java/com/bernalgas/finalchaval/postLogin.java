b1b88753f7b455b75e09225ceabad0e0e51d455b
Carlos Bernal <ramirez.bernal.jesus1@gmail.com>
2022-11-02 19:14:22 -0700

    notification sound and new logo added

--- app/src/main/java/com/bernalgas/finalchaval/postLogin.java
+++ app/src/main/java/com/bernalgas/finalchaval/postLogin.java
@@ -1,8 +1,26 @@
 package com.bernalgas.finalchaval;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationCompatExtras;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +34,8 @@ public class postLogin extends AppCompatActivity {
     //LoginResponse loginResponse;
     TextView u, e, n, b;
     FloatingActionButton noti,logout;
+    NotificationManagerCompat notificationManagerCompat;
+    Notification notification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,12 +76,81 @@ public class postLogin extends AppCompatActivity {
         });
 
         // TODO also eventually make it work the notification button
+
+        String tittle = "Titulo belico de notificacion";
+        String subject = "sujeto de la noti";
+        String body = "Cuerpo de la noti";
+
         noti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String msg = "Eventualmente funcionara";
-                Toast.makeText(postLogin.this, msg, Toast.LENGTH_SHORT).show();
+
+                //notificationManagerCompat.notify(1, notification);
+
+                createNotification();
+                // TODO custom notification sound
             }
         });
+
+        };
+
+    private void createNotification(){
+        String id = "My_channel_id_01";
+        Bitmap DONGOYO06 = ((BitmapDrawable)getDrawable(R.drawable.gosh_maquila)).getBitmap();
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Uri NATA = Uri.parse("android.resource://" + getApplicationContext().getPackageName() + "/" + R.raw.nataea);
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = manager.getNotificationChannel(id);
+            if (channel == null){
+                channel = new NotificationChannel(id,"Channel Tittle", NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription("[Channel description]");
+                channel.enableVibration(true);
+                channel.setBypassDnd(true);
+                channel.setVibrationPattern(new long[]{100,1000,200,300});
+                channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+                channel.setSound(NATA,audioAttributes);
+                manager.createNotificationChannel(channel);
+            }
+        }
+        // relleno del indio
+        // Intent notificationIntent = new Intent(this.)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,id)
+                .setSmallIcon(R.mipmap.ic_gosh_foreground)
+                .setContentTitle("Notificacion Belica")
+                .setContentText("Asi es, Don Gregorio 06 en tus notifaciones")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[]{100,1000,200,300})
+                .setAutoCancel(false)// true on notification menu dismised
+                .setTicker("Notification")
+                .setSound(NATA, AudioManager.STREAM_NOTIFICATION)
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(DONGOYO06));
+        NotificationManagerCompat m = NotificationManagerCompat.from(getApplicationContext());
+        m.notify(1,builder.build());
+    }
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 }
